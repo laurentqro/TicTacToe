d022e226830a7487361dc552faa174702b5f944e
Laurent Curau <laurentcurau@gmail.com>
2018-05-08 10:35:06 +0100

    Board can identify win

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Arrays;
 
 public class Board {
     private String[] grid;
@@ -9,5 +9,65 @@ public class Board {
 
     public String[] getGrid() {
         return grid;
+    }
+
+    public void setGrid(String[] grid) {
+        this.grid = grid;
+    }
+
+    public boolean hasWin() {
+        return (   lineHasWin(line1())
+                || lineHasWin(line2())
+                || lineHasWin(line3())
+                || lineHasWin(column1())
+                || lineHasWin(column2())
+                || lineHasWin(column3())
+                || lineHasWin(diagonal1())
+                || lineHasWin(diagonal2())
+        );
+    }
+
+    private boolean lineHasWin(String[] line) {
+        return Arrays.stream(line).distinct().count() == 1;
+    }
+
+    private String[] line1() {
+        String[] line = new String[]{this.grid[0], this.grid[1], this.grid[2]};
+        return line;
+    }
+
+    private String[] line2() {
+        String[] line = new String[]{this.grid[3], this.grid[4], this.grid[5]};
+        return line;
+    }
+
+    private String[] line3() {
+        String[] line = new String[]{this.grid[6], this.grid[7], this.grid[8]};
+        return line;
+    }
+
+    private String[] column1() {
+        String[] column = new String[]{this.grid[0], this.grid[3], this.grid[6]};
+        return column;
+    }
+
+    private String[] column2() {
+        String[] column = new String[]{this.grid[1], this.grid[4], this.grid[7]};
+        return column;
+    }
+
+    private String[] column3() {
+        String[] column = new String[]{this.grid[2], this.grid[5], this.grid[8]};
+        return column;
+    }
+
+    private String[] diagonal1() {
+        String[] diagonal = new String[]{this.grid[0], this.grid[4], this.grid[8]};
+        return diagonal;
+    }
+
+    private String[] diagonal2() {
+        String[] diagonal = new String[]{this.grid[2], this.grid[4], this.grid[6]};
+        return diagonal;
     }
 }
--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -7,4 +7,60 @@ public class BoardTest {
         Board board = new Board();
         assertArrayEquals(new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9"}, board.getGrid());
     }
+
+    @Test
+    public void boardHasWinLine1() {
+        Board board = new Board();
+        board.setGrid(new String[]{"X", "X", "X", "4", "5", "6", "7", "8", "9"});
+        assertTrue(board.hasWin());
+    }
+
+    @Test
+    public void boardHasWinLine2() {
+        Board board = new Board();
+        board.setGrid(new String[]{"1", "2", "3", "X", "X", "X", "7", "8", "9"});
+        assertTrue(board.hasWin());
+    }
+
+    @Test
+    public void boardHasWinLine3() {
+        Board board = new Board();
+        board.setGrid(new String[]{"1", "2", "3", "4", "5", "6", "X", "X", "X"});
+        assertTrue(board.hasWin());
+    }
+
+    @Test
+    public void boardHasWinColumn1() {
+        Board board = new Board();
+        board.setGrid(new String[]{"X", "2", "3", "X", "5", "6", "X", "8", "9"});
+        assertTrue(board.hasWin());
+    }
+
+    @Test
+    public void boardHasWinColumn2() {
+        Board board = new Board();
+        board.setGrid(new String[]{"1", "X", "3", "4", "X", "6", "7", "X", "9"});
+        assertTrue(board.hasWin());
+    }
+
+    @Test
+    public void boardHasWinColumn3() {
+        Board board = new Board();
+        board.setGrid(new String[]{"1", "2", "X", "4", "5", "X", "7", "8", "X"});
+        assertTrue(board.hasWin());
+    }
+
+    @Test
+    public void boardHasWinDiagonal1() {
+        Board board = new Board();
+        board.setGrid(new String[]{"X", "2", "3", "4", "X", "6", "7", "8", "X"});
+        assertTrue(board.hasWin());
+    }
+
+    @Test
+    public void boardHasWinDiagonal2() {
+        Board board = new Board();
+        board.setGrid(new String[]{"1", "2", "X", "4", "X", "6", "X", "8", "9"});
+        assertTrue(board.hasWin());
+    }
 }
